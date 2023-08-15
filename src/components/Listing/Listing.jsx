c8490d22aadeac4bf8bae37f3f9ab02f0c9df790
Sergei Korchagin <gamestationbase@gmail.com>
2023-08-15 14:55:47 +0300

    injustment work

--- src/components/Listing/Listing.jsx
+++ src/components/Listing/Listing.jsx
@@ -7,7 +7,7 @@ function Listing({ items }) {
                 <div className="item">
                     <div className="item-image">
                         <a href={item.url}>
-                            <img src="https://img1.etsystatic.com/156/0/12814579/il_570xN.1173240751_50hv.jpg" alt=""/>
+                            <img src={item.MainImage.url_570xN} alt=""/>
                         </a>
                     </div>
                     <div className="item-details">
